d3de00de8c9843989fdc404bc479a1f0720db76c
Bekar Shekiladze <bekar.shekiladze@leverx.com>
2026-01-08 09:58:33 +0400

    feat: add partial application implementation

--- src/utils/partialApplication.ts
+++ src/utils/partialApplication.ts
@@ -1,5 +1,8 @@
-export function partial(func, ...boundArgs) {
-  return function (...futureArgs) {
+export function partial<T, A extends unknown[], B extends unknown[], R>(
+  func: (...args: [...A, ...B]) => R,
+  ...boundArgs: A
+) {
+  return function (this: T, ...futureArgs: B): R {
     return func.call(this, ...boundArgs, ...futureArgs);
   };
 }
