056811b2f18294d583ebaffa24cd4e69fada9992
beqa <beqarioni@yahoo.co.uk>
2026-01-10 16:17:43 +0400

    fix: fix path mapping on base config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@lib': path.resolve(__dirname, 'src/index.ts'),
     },
   },
 });
